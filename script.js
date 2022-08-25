33df6d0c8daae19957aa3d5b5101cb7bd10d0aea
Emily <emily_nat12@icloud.com>
2022-08-24 23:41:17 -0400

    Changes button values to classes

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const gridContainer = document.getElementById("gridContainer");
 var rows = document.getElementsByClassName("gridRow");
 var gridBox = document.querySelectorAll(".gridBox");
 
-
 function makeGrid() {
-    makeRows(gridSize);
-    makeColumns(gridSize);
+    makeRows();
+    makeColumns();
     //Adds missing grid box in the bottom right corner
     var extra = document.createElement("div");
     extra.classList.add("gridBox");
@@ -13,6 +12,7 @@ function makeGrid() {
 }
 
 function makeRows(rowNum) {
+    gridSize();
     for (r = 0; r < rowNum; r++) {
         var row = document.createElement("div");
         row.classList.add("gridRow", "gridBox");
@@ -21,6 +21,7 @@ function makeRows(rowNum) {
 }
 
 function makeColumns(columnNum) {
+    gridSize();
     for (r = 0; r < rows.length; r++) {
         for (c = 0; c < columnNum; c++) {
             var column = document.createElement("div");
@@ -37,3 +38,23 @@ gridContainer.addEventListener('mouseover', function (e) {
         e.target.classList.add('gridColored');
       }
 });
+
+function gridSize() {
+    var btn = document.querySelectorAll("#gridBtn").value;
+    if (btn = "xs") {
+        makeRows(8);
+        makeColumns(8);
+    } else if (btn = "s") {
+        makeRows(16);
+        makeColumns(16);
+    } else if (btn = "m") {
+        makeRows(32);
+        makeColumns(32);
+    } else if (btn = "l") {
+        makeRows(64);
+        makeColumns(64);
+    } else if (btn = "xl") {
+        makeRows(124);
+        makeColumns(124);
+    }
+}
